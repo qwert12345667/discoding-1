e640273df32a02373c9cb095e6d6870dc51e60f7
fhfhfhfhgpdl <81856603+fhfhfhfhgpdl@users.noreply.github.com>
2021-09-08 22:17:37 +0900

    hosting(a) MainActivity add

---
+++ app/src/main/java/com/example/discoding/a_MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.discording_hostingpage
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.discoding.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.choose_spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.discording_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+        class SpinnerActivity : Activity(), AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+                // An item was selected. You can retrieve the selected item using
+                // parent.getItemAtPosition(pos)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Another interface callback
+            }
+        }
+
+        val items = resources.getStringArray(R.array.discording_array)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items)
+        spinner.adapter = adapter
+    }
+}
